7ddf6795e47dc14715fe90929c443c572faa89c2
Andrea Knabe <andrea.knabe@posteo.de>
2014-12-30 16:18:52 +0100

    Ergänzungen
    Hinzufügen von Erläuterungen, Hinzufügen einer Beispiel-XML-Datei für
    launchd (Mac OS)

--- fitbit_food_activities.py
+++ fitbit_food_activities.py
@@ -3,6 +3,7 @@
 import fitbit
 import json
 from pprint import pprint
+from twitter import *
 
 authd_client = fitbit.Fitbit('consumer-key', resource_owner_key='outh_token', resource_owner_secret='outh_token_secret')
 
@@ -12,15 +13,29 @@ carbs = food["summary"]["carbs"]
 calories = food["summary"]["calories"]
 verbraucht = verbrauchte_cal["summary"]["caloriesOut"]
 bilanz = calories - verbraucht
+dm = "DU SOLLTEST ETWAS ESSEN!"
+dm1 = "DU MUSST DRINGEND ETWAS ESSEN!"
+dm2 = "SO WIRD DAS NICHTS: ESSEN; DRINGEND!"
+dm4 = "CARBALARM: "
 
 print "Kohlenhydrate:  " + str(carbs)
 print "aufgenommene Kalorien: " + str(calories)
 print "verbrauchte Kalorien: " + str(verbraucht)
 print "Bilanz: " + str(calories - verbraucht)
-if bilanz < -250:
-	print "DU MUSST ESSEN!!!"
+
+if bilanz < -1000:
+	print dm2
+elif bilanz < -600:
+	print dm1
+elif bilanz < -250:
+	print dm
+else:
+	print "Alles im gruenen Bereich."
 	
-if bilanz < -250:
+if carbs > 20:
+	print dm4, carbs
+	
+if bilanz < -1000:
 	from twitter import *
 
 	t = Twitter(
@@ -28,8 +43,33 @@ if bilanz < -250:
     
 	t.direct_messages.new(
     user="@Benutzer",
-    text=(bilanz, dm, carbs))
+    text = (bilanz, dm2, carbs))   
+elif bilanz < -600:
+	from twitter import *
 
+	t = Twitter(
+	    auth=OAuth('Access Token', 'Access Token Secret', 'Consumer Key', 'Consumer Secret'))
+    
+	t.direct_messages.new(
+    user="@Benutzer",
+    text = (bilanz, dm1, carbs))   
+elif bilanz < -250:
+	from twitter import *
 
+	t = Twitter(
+	    auth=OAuth('Access Token', 'Access Token Secret', 'Consumer Key', 'Consumer Secret'))
+    
+	t.direct_messages.new(
+    user="@Benutzer",
+    text = (bilanz, dm, carbs))
 
+if carbs > 20:
+	from twitter import *
+
+	t = Twitter(
+	    auth=OAuth('Access Token', 'Access Token Secret', 'Consumer Key', 'Consumer Secret'))
+    
+	t.direct_messages.new(
+    user="@Benutzer",
+    text = (dm4, carbs))
 
