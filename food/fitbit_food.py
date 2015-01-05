f4f63d05601b3406275ef111f5b1aee6fd2145b3
Andrea Knabe <andrea.knabe@posteo.de>
2015-01-05 04:14:13 +0100

    code ergänzt

--- food/fitbit_food.py
+++ food/fitbit_food.py
@@ -9,16 +9,15 @@ authd_client = fitbit.Fitbit('zugangsdaten', resource_owner_key='zugangsdaten', 
 datum = raw_input("Datum im Format YYYY-MM-DD: ")
 
 food = authd_client.foods_log(date=datum, user_id=None, data=None)
-anzahl_dict = food 
-anzahl = len(anzahl_dict["foods"][0]["loggedFood"]["name"])
-print anzahl
+count_dict = food 
+y = len(count_dict["foods"])
+y = y - 1
+print y
 
-y = anzahl
-x = -1
+x = 0
 while x < y :
-	x = x + 1
 	name = food["foods"][x]["loggedFood"]["name"]
 	carbs = food["foods"][x]["nutritionalValues"]["carbs"]
 	liste = name, carbs
 	pprint(liste) 
-
+	x = x + 1
